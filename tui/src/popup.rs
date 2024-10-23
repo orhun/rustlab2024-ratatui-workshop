89a6c451d9685239484413dcf9b180addc72b9b9
Josh McKinney <joshka@users.noreply.github.com>
2024-10-23 07:19:52 -0700

    Make popup into a widget

--- tui/src/popup.rs
+++ tui/src/popup.rs
@@ -1,18 +1,42 @@
-use ratatui::layout::{Constraint, Flex, Layout, Rect};
+use ratatui::{
+    buffer::Buffer,
+    layout::{Constraint, Flex, Layout, Rect},
+    widgets::{Clear, StatefulWidget, Widget},
+};
 use ratatui_explorer::FileExplorer;
-use ratatui_image::protocol::StatefulProtocol;
+use ratatui_image::{protocol::StatefulProtocol, StatefulImage};
 
 pub enum Popup {
     FileExplorer(FileExplorer),
     ImagePreview(Box<dyn StatefulProtocol>),
 }
 
-impl Popup {
-    pub fn area(area: Rect, percent_x: u16, percent_y: u16) -> Rect {
-        let vertical = Layout::vertical([Constraint::Percentage(percent_y)]).flex(Flex::Center);
-        let horizontal = Layout::horizontal([Constraint::Percentage(percent_x)]).flex(Flex::Center);
-        let [area] = vertical.areas(area);
-        let [area] = horizontal.areas(area);
-        area
+impl Widget for &mut Popup {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        match self {
+            Popup::FileExplorer(explorer) => render_explorer(area, buf, explorer),
+            Popup::ImagePreview(ref mut protocol) => render_image_preview(area, buf, protocol),
+        }
     }
+}
+
+fn render_explorer(area: Rect, buf: &mut Buffer, explorer: &mut FileExplorer) {
+    let popup_area = popup_area(area, 50, 50);
+    Clear.render(popup_area, buf);
+    explorer.widget().render(popup_area, buf);
+}
+
+fn render_image_preview(area: Rect, buf: &mut Buffer, protocol: &mut Box<dyn StatefulProtocol>) {
+    let popup_area = popup_area(area, 80, 80);
+    Clear.render(popup_area, buf);
+    let image = StatefulImage::new(None);
+    image.render(popup_area, buf, protocol);
+}
+
+fn popup_area(area: Rect, percent_x: u16, percent_y: u16) -> Rect {
+    let vertical = Layout::vertical([Constraint::Percentage(percent_y)]).flex(Flex::Center);
+    let horizontal = Layout::horizontal([Constraint::Percentage(percent_x)]).flex(Flex::Center);
+    let [area] = vertical.areas(area);
+    let [area] = horizontal.areas(area);
+    area
 }
--- tui/src/ui.rs
+++ tui/src/ui.rs
@@ -1,11 +1,9 @@
 use ratatui::layout::{Constraint, Layout};
 use ratatui::text::Line;
-use ratatui::widgets::{Block, Clear};
+use ratatui::widgets::Block;
 use ratatui::Frame;
-use ratatui_image::StatefulImage;
 
 use crate::app::App;
-use crate::popup::Popup;
 
 impl App {
     pub fn draw_ui(&mut self, frame: &mut Frame) {
@@ -28,15 +26,8 @@ impl App {
         frame.render_widget(&mut self.message_list, message_area);
         frame.render_widget(&mut self.room_list, room_area);
 
-        if let Some(Popup::FileExplorer(ref explorer)) = self.popup {
-            let popup_area = Popup::area(frame.area(), 50, 50);
-            frame.render_widget(Clear, popup_area);
-            frame.render_widget(&explorer.widget(), popup_area);
-        } else if let Some(Popup::ImagePreview(protocol)) = &mut self.popup {
-            let popup_area = Popup::area(frame.area(), 80, 80);
-            frame.render_widget(Clear, popup_area);
-            let image = StatefulImage::new(None);
-            frame.render_stateful_widget(image, popup_area, protocol);
+        if let Some(popup) = &mut self.popup {
+            frame.render_widget(popup, frame.area());
         }
     }
 }
