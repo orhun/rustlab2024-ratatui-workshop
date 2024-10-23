342d706bda8eee49c0d3dc330bb5e69cb242dddd
Josh McKinney <joshka@users.noreply.github.com>
2024-10-23 07:10:18 -0700

    Move args to main.rs
    Small files can be annoying to have to flip between

--- tui/src/main.rs
+++ tui/src/main.rs
@@ -1,12 +1,16 @@
+use clap::Parser;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+
 mod app;
-mod args;
 mod message_list;
 mod popup;
 mod room_list;
 mod ui;
 
 use app::App;
-use args::Args;
+
+pub const DEFAULT_IP: IpAddr = IpAddr::V4(Ipv4Addr::LOCALHOST);
+pub const DEFAULT_PORT: u16 = 42069;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -16,4 +20,20 @@ async fn main() -> anyhow::Result<()> {
     let result = app.run(terminal).await;
     ratatui::restore();
     result
+}
+
+#[derive(Parser)]
+pub struct Args {
+    #[arg(short, long, default_value_t = DEFAULT_IP)]
+    ip: IpAddr,
+
+    #[arg(short, long, default_value_t = DEFAULT_PORT)]
+    port: u16,
+}
+
+impl Args {
+    pub fn parse_socket_addr() -> SocketAddr {
+        let cli = Self::parse();
+        SocketAddr::new(cli.ip, cli.port)
+    }
 }
