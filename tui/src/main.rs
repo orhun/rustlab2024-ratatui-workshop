a9af841a892d77e1c33ff47dc47b3f6ab9ef03ef
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2024-11-05 19:04:59 +0300

    Add a simple test (solution)

--- tui/src/main.rs
+++ tui/src/main.rs
@@ -81,6 +81,12 @@ mod tests {
 
     #[tokio::test]
     async fn test_render_app() -> anyhow::Result<()> {
-        todo!("write test")
+        let addr = Args::default();
+        let addr = SocketAddr::new(addr.ip, addr.port);
+        let mut app = App::new(addr);
+        let mut terminal = Terminal::new(TestBackend::new(80, 20)).unwrap();
+        terminal.draw(|frame| app.draw_ui(frame))?;
+        assert_snapshot!(terminal.backend());
+        Ok(())
     }
 }
